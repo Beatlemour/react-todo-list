346ec17608ede43ce79d3e2458b02a19c4764e14
unknown <aashish.maharjan.mails@gmail.com>
2020-09-06 18:04:29 +1000

    Updates

---
+++ precache-manifest.6b6b75ebd3476ec49d86127837120cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3176609110d9c8efc2eed71837cf74ab",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "c92cc55767fb742e96f8",
+    "url": "/react-todo-list/static/css/main.573dd2e6.chunk.css"
+  },
+  {
+    "revision": "6fc3a4e870883100d9b8",
+    "url": "/react-todo-list/static/js/2.7fe5cf45.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-list/static/js/2.7fe5cf45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92cc55767fb742e96f8",
+    "url": "/react-todo-list/static/js/main.948e7326.chunk.js"
+  },
+  {
+    "revision": "2c6e5d8f9eaaedf40455",
+    "url": "/react-todo-list/static/js/runtime-main.f23babde.js"
+  }
+]);
